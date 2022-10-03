ded35127d7d203c8e97ae7530337d398a4cdca9a
[klisman28] <klisoved28@gmail.com>
2022-10-03 10:58:38 -0600

    instalación de axios

--- Energi/src/components/Video.vue
+++ Energi/src/components/Video.vue
@@ -5,7 +5,7 @@
                 <div class="col-lg-6 order-2 order-lg-1">
                     <div class="video-player">
                         <figure class="ratio ratio-16x9">
-                            <iframe src="https://youtu.be/BL8z5kHLezE" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <iframe src="https://www.youtube.com/watch?v=51h76yjA3wQ&ab_channel=ISOY" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                         </figure>
                     </div>
                 </div>
